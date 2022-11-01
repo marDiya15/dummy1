0999dbfde4f9aabf3e778cc3c6b3e2680696e279
marDiya15 <115971271+marDiya15@users.noreply.github.com>
2022-11-01 10:31:36 +0800

    init push

--- moment.js
+++ moment.js
@@ -10945,7 +10945,7 @@
         months: 'Januari_Februari_Mac_April_Mei_Jun_Julai_Ogos_September_Oktober_November_Disember'.split('_'),
         monthsShort: 'Jan_Feb_Mac_Apr_Mei_Jun_Jul_Ogs_Sep_Okt_Nov_Dis'.split('_'),
         weekdays: 'Ahad_Isnin_Selasa_Rabu_Khamis_Jumaat_Sabtu'.split('_'),
-        weekdaysShort: 'Ahd_Isn_Sel_Rab_Kha_Jum_Sab'.split('_'),
+        weekdaysShort: 'Ahad_Isnin_Selasa_Rabu_Khamis_Jumaat_Sabtu'.split('_'),
         weekdaysMin: 'Ah_Is_Sl_Rb_Km_Jm_Sb'.split('_'),
         longDateFormat: {
             LT: 'HH.mm',
